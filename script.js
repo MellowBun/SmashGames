7d2cf67b60a590ea4fd18f45793135d45f71ff4b
devan.couch <couch.devan@gmail.com>
2023-05-25 11:55:07 -0700

    added script for cards

---
+++ script.js
@@ -0,0 +1,25 @@
+const data = {
+    cards: [
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alt text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html",
+        },
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alt text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html",
+        },
+        {
+            imgPath: "./somewhere.img",
+            imgAltText: "some alt text",
+            title: "The Title",
+            body: "some kind of body of text",
+            actionLink: "https://localhost:5500/games/game.html",
+        },
+    ]
+}
