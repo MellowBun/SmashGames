dcb8eef3fb9375d9f54143d7873dc9428bd9c37e
devan.couch <couch.devan@gmail.com>
2023-05-31 11:55:53 -0700

    added deck and cards to script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// index.html Smash Games - Home
+// blast.html Smash Games - Inferno Blast
+
 const data = {
     brandName: "Smash Games",
     pages: [
         {
-            name: "index", 
+            name: "index",
             pageName: "Home",
             callToAction: {
                 src: "images/SmashGamesLogo-short.png",
@@ -10,11 +13,50 @@ const data = {
             }
         },
         {
-            name: "blast", 
+            name: "blast",
             pageName: "Inferno Blast",
-            callToAction: {
-                src: "images/Inferno-Jumbotron.png"
-            }
+            blocks: [
+                { 
+                    type: "call-to-action",
+                    src: "images/Inferno-Jumbotron.png",
+                    altText: "Inferno Blast Gameplay",
+                    buttonLinkSrc: "https://steampowered.com",
+                    buttonLinkText: "Buy now on Steam"
+                },
+                {
+                    type: "description",
+                    text: "text here"
+                },
+                {
+                    type: "deck",
+                    cards: [
+                        {
+                            type: "card",
+                            src: "images/Inferno/Inferno-2-Short.png",
+                            altText: "Inferno Blast Gameplay",
+                            title: "Dynamic Environments",
+                            body: "Navigate through a variety of fiery landscapes, including lava pits, crumbling ruins, and demonic strongholds."
+                        },
+                        {
+                            type: "card",
+                            src: "images/Inferno/Inferno-1-Short.png",
+                            altText: "Inferno Blast Gameplay",
+                            title: "Upgradeable Weapons",
+                            body: "Customize your arsenal with a range of weapons and power-ups, including flamethrowers, rocket launchers, and more.",
+                        },
+                        {
+                            type: "card",
+                            src: "images/Inferno/Inferno-3-Short.png",
+                            altText: "Inferno Blast Gameplay",
+                            title: "Epis Boss Battles",
+                            body: "Face off against towering demon bosses, each with their own unique strengths and weaknesses.",
+                        }
+                    ],
+
+                }
+            ],
+
         }
     ]
+    
 }
